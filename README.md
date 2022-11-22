d8f25031c1df15aade289ea287e7b130b14f9d14
Alessandro Russo <axrusso@amazon.com>
2022-11-22 09:34:31 +0100

    Updating README.md and main script

--- README.md
+++ README.md
@@ -1,17 +1,50 @@
-## My Project
+# AWS Organizations Alternate Contacts management via CSV
 
-TODO: Fill this README out!
+Nowadays, customers have several linked accounts in their [AWS Organizations](https://aws.amazon.com/organizations/). These linked accounts might require different alternate contacts for many reasons and keeping such contacts updated is fundamental. Unfortunately, populating such contacts might be a complex and time-consuming activity. Customers would like to fill in their AWS linked accounts alternate contacts in a simple and quick way, closer to their daily way of working, like exporting to a CSV file, modifying it keeping the original formatting, and importing the updated contacts from the management account.
 
-Be sure to:
+This work has been inspired by the blog post [Programmatically managing alternate contacts on member accounts with AWS Organizations](https://aws.amazon.com/blogs/mt/programmatically-managing-alternate-contacts-on-member-accounts-with-aws-organizations/)
 
-* Change the title in this README
-* Edit your repository description on GitHub
+## Requirements
+
+- The AWS Organizations must have all features enabled, please see [Enabling all features in your organization](https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_org_support-all-features.html).
+- You need to enable trusted access with AWS Organizations for AWS Account Management, please see [Enabling trusted access for AWS Account Management](https://docs.aws.amazon.com/accounts/latest/reference/using-orgs-trusted-access.html).
+- Only the AWS Organizations management account can export and import the linked accounts alternate contacts.
+
+## Overview
+
+The script leverages on AWS CLI 2.0 and AWS CloudShell to enable the AWS Organizations management account to easily export all the linked accounts alternate contacts to a regular CSV file. Then, the file can be integrated or updated, and uploaded again. *Please keep in mind that the CSV format has to be kept to make it works properly*, thus avoid additional comma and pay attention to special characters.
+
+## Things to know 
+
+1. Each account/contact type in the CSV file has to provide all the corresponding alternate contacts details, in case any of them will be missed that entry will be skipped. 
+2. We recommend to limit the CSV file to only those rows that need to be updated. 
+3. When you export it, feel free to create a copy for backup
+
+## Usage
+
+- From the AWS Organizations management account run AWS CloudShell
+- Upload the script alternate-contacts-to-from-csv.sh into AWS CloudShell
+- Run the script to export the alternate contacts, as follow
+
+```bash
+$ alternate-contacts-to-from-csv.sh -o aws-alternate-contacts.csv
+```
+
+- From AWS CloudShell you can download the resulting CSV file, keeping a copy as a backup. Review and integrate the CSV file: each account entry has to report all the corresponding alternate contacts details, otherwise it will not be imported. We recommend to limit the CSV file to only those rows that need to be updated
+- (Optional) Review the differences between the original export the CSV to import, being sure of the changes made
+- Finally, upload the CSV file into AWS CloudShell and run the import command, as follow
+
+```bash
+$ alternate-contacts-to-from-csv.sh -i aws-alternate-contacts.csv
+```
+
+Simple but powerful.
 
 ## Security
 
 See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
 
+
 ## License
 
-This library is licensed under the MIT-0 License. See the LICENSE file.
-
+This solution is licensed under the MIT-0 License. See the LICENSE file.
---
+++ alternate-contacts-to-from-csv.sh
@@ -0,0 +1,157 @@
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+#!/bin/bash
+
+output_file=""
+source_file=""
+max_retry=4
+exit_code=0
+loaded=0
+skippe=0
+
+usage() 
+{ 
+  echo "$0: missing operand"
+  echo "Usage is: $0 [-i|-o] <alternate-contacts-file.csv>" 
+  echo "-i to import the alternate contact from the given csv file"
+  echo "-o to export the actual alternate contacts into a csv file"
+  exit 1;
+}
+
+while getopts "i:o:" options; do
+case "${options}" in
+i)
+	source_file=${OPTARG}
+	;;     
+o)
+	output_file=${OPTARG}
+	;;     
+*)
+	usage
+	;;
+esac
+done
+shift $((OPTIND-1))
+
+if [ -z "$source_file" -a -z "$output_file" ]; then 
+	usage
+fi
+
+#if [ ! -z "${source_file}" -a ! -f "${source_file}" ]; then
+#	echo "File \"$source_file\" does not exist...exit" 
+#	exit 1
+#fi
+
+management_account=`aws organizations describe-organization --query Organization.MasterAccountId --output text` 2>&1 1>$0.log
+if [ $? -ne "0" ]; then
+	echo "Error in fetching master account id"
+	exit 1
+fi
+#echo "Management account is \"$management_account\""
+	
+ 
+if [ ! -z "$output_file" ]; then
+	output_file="$PWD/$output_file"
+	echo "Listing all accounts in \"$output_file\""
+	[ -f "$output_file" ] && echo "File \"$output_file\" exists, please rename/backup/remove it before proceeding" && exit 2
+	touch $output_file
+	for account_id in $(aws organizations list-accounts --query 'Accounts[].Id' --output text); 
+	do 
+		echo -n "Account $account_id: "
+		for type in OPERATIONS SECURITY BILLING;
+		do
+			counter=0
+			exit_code=0
+			echo -n "$type..."
+			echo -n "$account_id," >>$output_file
+			while [ $counter -lt $max_retry ]
+			do
+			  contact=$(aws account get-alternate-contact --account-id ${account_id} --alternate-contact-type $type --output text 2>/dev/null)
+			  exit_code=$?
+			  if [ $exit_code -eq "0" ]; then
+				echo "$contact" | tr "\t" "," >>$output_file 
+				break;
+  			  elif [ $exit_code -eq 254 ]; then
+				echo "ALTERNATECONTACT,$type,,,," >> $output_file
+				break;
+			  fi
+  	 		  echo "Error with exit code=$exit_code while executing ($counter/$max_retry):" >>$0.log
+			  echo -e "\taws account get-alternate-contact --account-id ${account_id} --alternate-contact-type $type --output text" >>$0.log
+			  ((counter++))
+			  sleep .2
+			done
+			if [ $counter -eq $max_retry ]; then
+				echo "Error with exit code=$exit_code while executing:" >>$0.log
+				echo -e "\taws account get-alternate-contact --account-id ${account_id} --alternate-contact-type $type --output text" >>$0.log
+				exit 1
+			fi
+		done
+		echo "done"
+	done
+else
+	source_file=$PWD/$source_file
+	[ ! -f "$source_file" ] && echo "File \"$source_file\" does not exist, please populate it before proceeding using it" && usage
+	comma_check=$(grep -o "," $source_file|wc -m)
+	comma_check=$(expr $comma_check % 6 ) # no more than six comma per row
+	[ $comma_check -ne "0" ] && echo "There are too commas in \"$source_file\" which should be multiple of six: check and fix it." && exit 1
+	# echo "Loading accounts and alternate contacts from \"$source_file\""
+	IFS=','
+        while read -r account_id other type account_email account_name account_phone account_title; do
+		number='^[0-9]+$'
+		echo -n "Working on account \"$account_id\": "
+		if ! [[ $account_id =~ $number ]] ; then
+			echo  "Skipping invalid account id."
+			continue
+		fi
+		if [ "$management_account" -eq "$account_id" ]; then
+			echo "This is the management account and will be skipped."
+			continue
+		fi
+		if [ -z "$account_email" -o -z "$account_name" -o -z "$account_phone" -o -z "$account_title" ]; then
+			echo "Skipped - some data is missed, check $0.log" # All contacts details have to be provided
+			echo "Missed data for account \"$account_id\" for \"$type\" is missed and it will be skipped: account_email=\"$account_email\" account_name=\"$account_name\" account_phone=\"$account_phone\" account_title=\"${account_title}\"">>$0.log
+			((skipped++))
+			continue
+		fi
+		counter=0
+		exit_code=0
+		while [ $counter -lt $max_retry ]
+		do
+			# Name, phone, email, and title must be provided for BILLING, OPERATIONS, SECURITY
+			aws account put-alternate-contact --account-id ${account_id} --alternate-contact-type ${type} --email-address ${account_email} --name ${account_name} --phone-number ${account_phone} --title ${account_title}
+			exit_code=$?
+      			if [ $exit_code -ne "0" ]; then
+				echo "Error with exit code=$exit_code while executing ($counter/$max_retry):" >>$0.log
+				echo -e "\taws account put-alternate-contact --account-id ${account_id} --alternate-contact-type ${type} --email-address ${account_email} --name ${account_name} --phone-number ${account_phone} --title ${account_title}">>$0.log
+				((counter++))
+	      			sleep .2
+			else
+				echo "$type done"
+				((loaded++))
+				break
+			fi
+		done
+		if [ $counter -eq $max_retry ]; then
+			echo "Error with exit code=$exit_code while executing:" >>$0.log
+			echo -e "\taws account get-alternate-contact --account-id ${account_id} --alternate-contact-type $type --output text" >>$0.log
+			exit 1
+		fi
+	done < $source_file
+	echo "Contacts loaded:$loaded, skipped:$skipped ."
+fi
